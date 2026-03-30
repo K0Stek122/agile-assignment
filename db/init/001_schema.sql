8a725fbb9b10d267f61c5f1edfd436fa32b8ddcc
JVgt-hb <jamievillarroel0@gmail.com>
2026-03-30 13:07:47 +0100

    fixed some datatypes

--- db/init/001_schema.sql
+++ db/init/001_schema.sql
@@ -6,32 +6,31 @@ CREATE TABLE User (
                               NOT NULL,
     [User name]       TEXT    NOT NULL,
     [User type]       TEXT    NOT NULL,
-    Email             TEXT    NOT NULL,
+    [Email]             TEXT    NOT NULL,
     [Phone number]    INTEGER NOT NULL,
     [Membership type] TEXT    NOT NULL
 );
 
 CREATE TABLE Schedule (
-    ScheduleInstanceID  INTEGER NOT NULL
+    [ScheduleInstanceID]  INTEGER NOT NULL
                                 PRIMARY KEY AUTOINCREMENT,
     [User ID (member)]  TEXT    REFERENCES User ([User ID]) 
                                 NOT NULL,
     [User ID (trainer)] TEXT    REFERENCES User ([User ID]) 
                                 NOT NULL,
-    Time                TEXT    NOT NULL,
-    Date                TEXT    NOT NULL
+    [Date/time]         DATETIME    NOT NULL
 );
 
 CREATE TABLE Parking (
-    ParkingInstanceID INTEGER PRIMARY KEY AUTOINCREMENT
+    [ParkingInstanceID] INTEGER PRIMARY KEY AUTOINCREMENT
                               UNIQUE
                               NOT NULL,
     [User ID]         INTEGER REFERENCES User ([User ID]) 
                               NOT NULL,
     [License plate]   TEXT    NOT NULL,
-    Payment           NUMERIC NOT NULL,
-    [Start timestamp] NUMERIC NOT NULL,
-    [End timestamp]   NUMERIC NOT NULL
+    [Payment]           NUMERIC NOT NULL,
+    [Start timestamp] DATETIME NOT NULL,
+    [End timestamp]   DATETIME NOT NULL
 );
 
 CREATE TABLE [Credit Card Information] (
@@ -41,6 +40,6 @@ CREATE TABLE [Credit Card Information] (
     [User ID]     INTEGER NOT NULL
                           REFERENCES User ([User ID]),
     [Card number] INTEGER NOT NULL,
-    Expiry        NUMERIC NOT NULL,
-    CVC           INTEGER NOT NULL
+    [Expiry]        TEXT NOT NULL,
+    [CVC]           INTEGER NOT NULL
 );
