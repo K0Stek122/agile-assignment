60cb9503002b41a081c10bb424beff29f74b972a
K0Stek122 <kamilianos3@gmail.com>
2026-05-05 11:14:00 +0100

    Account-mgmt almost done. added delete-user endpoi
    Signed-off-by: K0Stek122 <kamilianos3@gmail.com>

--- account-mgmt/app.py
+++ account-mgmt/app.py
@@ -41,5 +41,13 @@ def get_user_membership_type(user_id):
     if response.status_code != 200:
         return response.text, response.status_code
     return {'membership_type': response.json()['membership_type']}, 200
+
+@app.route('/api/account-mgmt/delete-user/<int:user_id>')
+def delete_user(user_id):
+    response = requests.get(f"http://db-api:5431/api/db-api/")
+    if response.status_code != 200:
+        return response.text, response.status_code
+    return 'User deleted', 200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5432)
